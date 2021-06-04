4b77581b6fd9bdb2cc0c831a42a9d2eb224d0418
Peter Henell <mail@peterhenell.se>
2021-06-04 20:21:49 +0200

    adding spark configuration file, disabling s3 cert check until we fix self signed minio access

--- s3.scala
+++ s3.scala
@@ -1,10 +1,3 @@
-// sc.hadoopConfiguration.set("spark.hadoop.fs.s3a.endpoint", "http://minio:9000")
-// sc.hadoopConfiguration.set("spark.hadoop.fs.s3a.access.key", "ABCDE123")
-// sc..hadoopConfiguration.set("spark.hadoop.fs.s3a.secret.key", "ABCDE123")
-// sc..hadoopConfiguration.set("spark.hadoop.fs.s3a.path.style.access", "true")
-// sc..hadoopConfiguration.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-
-
 // Tested working with public https://play.min.io:9000 Q3AM3UQ867SPQQA43P2F zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG
 sc.hadoopConfiguration.set("fs.s3a.endpoint", "https://play.min.io:9000")
 sc.hadoopConfiguration.set("fs.s3a.access.key", "Q3AM3UQ867SPQQA43P2F")
@@ -15,13 +8,11 @@ sc.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSyste
 // might need this for our installation to begin with
 sc.hadoopConfiguration.set("fs.s3a.connection.ssl.enabled", "false")
 
+// for all options (lol)
+// https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-common/core-default.xml
 
 val myRDD = sc.textFile("s3a://documents/Notebook-demo.txt")
 myRDD.count
-
-//sc.hadoopConfiguration.set("fs.s3a.access.key", "minioadmin")
-//sc.hadoopConfiguration.set("fs.s3a.secret.key", "minioadmin")
-
 
 // for own minio installation
 sc.hadoopConfiguration.set("fs.s3a.endpoint", "https://minio")
